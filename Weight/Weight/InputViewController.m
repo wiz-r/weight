06539b7f8979bf0a5bfe8e36cddfca4a5c7188f2
Takuya Watabe <takuya.watabe@gmail.com>
2012-11-15 23:12:58 +0900

    input view ui beta

--- Weight/Weight/InputViewController.m
+++ Weight/Weight/InputViewController.m
@@ -9,7 +9,15 @@
 #import "InputViewController.h"
 
 @interface InputViewController ()
+- (IBAction)datePickerChanged:(id)sender;
 - (IBAction)cancelButtonPushed:(id)sender;
+- (IBAction)dateButtonPushed:(id)sender;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UIButton *dateButton;
+- (IBAction)closeSoftwareKeyboard:(id)sender;
+@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
+@property (weak, nonatomic) IBOutlet UIButton *okButton;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *cancelButton;
 
 @end
 
@@ -24,10 +32,19 @@
     return self;
 }
 
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+   
+    if ([self.textField canBecomeFirstResponder])
+    {
+        [self.textField becomeFirstResponder];
+    }
+    self.textField.returnKeyType = UIReturnKeyDone;
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
 }
 
 - (void)didReceiveMemoryWarning
@@ -36,9 +53,25 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)datePickerChanged:(id)sender {
+    NSDate* date = self.datePicker.date;
+    NSDateFormatter* formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"YYYY/MM/dd"];
+    [self.dateButton setTitle:[formatter stringFromDate:date] forState:UIControlStateNormal];
+}
+
 - (IBAction)cancelButtonPushed:(id)sender {
     [self dismissViewControllerAnimated:YES completion:^{
         ;
     }];
+}
+
+- (IBAction)dateButtonPushed:(id)sender {
+    [self.textField resignFirstResponder];
+}
+- (IBAction)closeSoftwareKeyboard:(id)sender {
+    [self.textField resignFirstResponder];
+}
+- (IBAction)cancel:(id)sender {
 }
 @end
