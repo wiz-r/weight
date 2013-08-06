ddd5bda6fb6b2e8028e0f03a037c7d0e092b63a1
Takuya Watabe <takuya.watabe@gmail.com>
2013-08-06 11:01:07 +0900

    initial commit of android

---
+++ src/android/WeightLoggerProject/WeightLogger/src/main/java/com/wizr/weightlogger/fragment/MainFragment.java
@@ -0,0 +1,23 @@
+package com.wizr.weightlogger.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.wizr.weightlogger.R;
+
+/**
+ * Created by takuya.watabe on 8/6/13.
+ */
+public class MainFragment extends Fragment {
+    public MainFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+}
