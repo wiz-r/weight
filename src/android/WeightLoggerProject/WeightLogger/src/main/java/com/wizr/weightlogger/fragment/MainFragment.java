6cdd5f2b7d43f777f27f3c23dc7f61af06a61922
Takuya Watabe <takuya.watabe@gmail.com>
2013-12-22 17:47:58 +0900

    notification sample

--- src/android/WeightLoggerProject/WeightLogger/src/main/java/com/wizr/weightlogger/fragment/MainFragment.java
+++ src/android/WeightLoggerProject/WeightLogger/src/main/java/com/wizr/weightlogger/fragment/MainFragment.java
@@ -1,5 +1,11 @@
 package com.wizr.weightlogger.fragment;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -32,7 +38,37 @@ public class MainFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 MainActivity activity = (MainActivity)getActivity();
+                /*
                 activity.showInputDialog();
+                */
+
+                PendingIntent contentIntent;
+                contentIntent = PendingIntent.getActivity(activity, 0,
+                        new Intent(activity, MainActivity.class), 0);
+
+                Notification noti;
+                noti =
+                        new Notification.BigPictureStyle(
+                        //new Notification.BigTextStyle(
+                        new Notification.Builder(activity)
+                                .setContentTitle("Content Title eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")
+                                .setContentText("Content Text dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd")
+                                .setSmallIcon(R.drawable.ic_launcher)
+                                .setLargeIcon(
+                                    BitmapFactory.decodeResource(getResources(), R.drawable.logo)
+                                )
+                //)
+                //        ).bigText("hogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehoge")
+                        ).bigPicture(
+                                BitmapFactory.decodeResource(getResources(), R.drawable.kyojin)
+                        ).setBigContentTitle("Big Content Title ぜぜぜぜぜぜぜぜぜぜぜぜぜぜぜぜぜぜぜぜぜぜぜぜ")
+                        .setSummaryText("Summary Text さささささささささささささささささささささささささささささささささささ")
+                        .bigLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.logo))
+                        .build()
+                ;
+
+                NotificationManager nm = (NotificationManager) activity.getSystemService(Context.NOTIFICATION_SERVICE);
+                nm.notify(1, noti);
             }
         });
 
